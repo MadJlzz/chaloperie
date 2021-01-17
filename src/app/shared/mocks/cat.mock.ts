88e0cfd71828e2f24037efdf04ad55cef44b2db8
Julien Klaer <klaer.julien@gmail.com>
2021-01-17 23:02:06 +0100

    Progressing.

--- src/app/shared/mocks/cat.mock.ts
+++ src/app/shared/mocks/cat.mock.ts
@@ -4,13 +4,19 @@ export const CATS: Cat[] = [
   {
     title: 'Shadow does HAI',
     pictureUrl: '/assets/placeholder/hai_cat.jpg',
-    description: 'Shadow does the HAI',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempor sit amet velit ut imperdiet. Aliquam aliquam auctor posuere. In dui nisi, elementum non lacus sed, feugiat semper odio. Mauris vitae sapien lacus. Aliquam ultricies erat at turpis lacinia cursus. Sed sodales quam in purus elementum eleifend. Cras congue efficitur nisl at interdum. Curabitur mattis augue purus, sit amet dapibus dui maximus ultricies. Vivamus non urna malesuada, ornare neque id, aliquam nibh. Cras interdum lacus sed bibendum posuere.\n',
     date: '14th of January 2021'
   },
   {
-    title: 'Shadow does HAI',
+    title: 'One does not simply DOGE',
+    pictureUrl: 'https://material.angular.io/assets/img/examples/shiba1.jpg',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id neque metus. Proin pellentesque, nunc quis gravida congue, eros risus fringilla libero, sed fermentum orci ligula sit amet justo. Nullam massa sapien, mollis id velit sollicitudin, suscipit fermentum nulla. Fusce aliquam mi in sapien mattis vehicula suscipit maximus ex. Ut in elit velit. Nullam gravida blandit interdum. Donec in nunc luctus, convallis lorem vitae, viverra mauris. Quisque tempor urna urna, a condimentum odio pharetra nec. Nulla ut enim commodo, feugiat magna id, volutpat lacus. Integer hendrerit aliquam magna sit amet lacinia. Sed ac dolor vel velit ullamcorper semper. Nunc suscipit odio non velit molestie feugiat.\n',
+    date: '15th of January 2021'
+  },
+  {
+    title: 'DOGE goes WOW',
     pictureUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-    description: 'Shadow does the HAI',
-    date: '14th of January 2021'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et fermentum lectus, nec cursus tortor. Vestibulum scelerisque, velit id gravida vestibulum, enim sem scelerisque ante, vel fermentum mi turpis ut libero. Duis id ultricies lectus. Nam at ligula vel ligula tincidunt maximus ac sed ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent orci purus, aliquam vitae mattis sit amet, commodo eu arcu. Vestibulum euismod, quam non hendrerit posuere, magna quam dignissim risus, ullamcorper cursus dui libero eget massa.\n',
+    date: '16th of January 2021'
   }
 ];
